e18552f61b86fef71002c312530af59fd2911596
zyllle <zyll3on@gmail.com>
2019-10-06 11:15:40 -0600

    Initial commit

---
+++ Source/AntiniumRaceCode/HarmonyPatches.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using RimWorld;
+using Verse;
+using Harmony;
+
+
+
+namespace AntiniumRaceCode
+{
+
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        // this static constructor runs to create a HarmonyInstance and install a patch.
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.antinium");
+
+            // find the AddFoodPoisoningHediff method of the class RimWorld.FoodUtility
+            MethodInfo targetmethod = AccessTools.Method(typeof(RimWorld.FoodUtility), "AddFoodPoisoningHediff");
+
+            // find the static method to call before (i.e. Prefix) the targetmethod
+            HarmonyMethod prefixmethod = new HarmonyMethod(typeof(AntiniumRaceCode.HarmonyPatches).GetMethod("AddFoodPoisoningHediff_Prefix"));
+
+            // patch the targetmethod, by calling prefixmethod before it runs, with no postfixmethod (i.e. null)
+            harmony.Patch(targetmethod, prefixmethod, null);
+
+            targetmethod = AccessTools.Method(typeof(RimWorld.FoodUtility), "AddIngestThoughtsFromIngredient");
+            prefixmethod = new HarmonyMethod(typeof(AntiniumRaceCode.HarmonyPatches).GetMethod("AddIngestThoughtsFromIngredient_Prefix"));
+            harmony.Patch(targetmethod, prefixmethod, null);
+        }
+
+        public static void AddIngestThoughtsFromIngredient_Prefix(ThingDef ingredient, Pawn ingester, List<ThoughtDef> ingestThoughts)
+        {
+
+            if (ingester.story.traits.allTraits.Any(tr => tr.def.defName == "Ant_BirdLover"))
+            {
+                if (ingredient.ingestible.sourceDef.race.body.defName == "Bird" || ingredient.ingestible.sourceDef.race.leatherDef.defName == "Leather_Bird")
+                {
+                    // ingestThoughts.Add();                // TODO: figure out how to identify my new thought so it can be added
+
+                    
+                }
+
+            }
+
+
+
+
+
+            if (ingester.RaceProps.Humanlike && FoodUtility.IsHumanlikeMeat(ingredient))
+            {
+                ingestThoughts.Add((!ingester.story.traits.HasTrait(TraitDefOf.Cannibal)) ? ThoughtDefOf.AteHumanlikeMeatAsIngredient : ThoughtDefOf.AteHumanlikeMeatAsIngredientCannibal);
+            }
+            else if (ingredient.ingestible.specialThoughtAsIngredient != null)
+            {
+                ingestThoughts.Add(ingredient.ingestible.specialThoughtAsIngredient);
+            }
+        }
+
+        // This method is now always called right before RimWorld.FoodUtility.AddFoodPoisoningHediff.
+        public static bool AddFoodPoisoningHediff_Prefix(Pawn pawn)
+        {
+            if (pawn.kindDef.race.defName == "Ant_AntiniumRace")
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+
+    }
+}
