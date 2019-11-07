055b1e8379d1152ef479eedb6b3b3266fef5b457
zyllle <zyll3on@gmail.com>
2019-11-07 15:36:06 -0700

    .

---
+++ Source/AntHiveQueen/AntHQDefOf.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace AntHiveQueen
+{
+
+    [DefOf]
+    public static class AntHQDefOf
+    {
+        public static PawnKindDef Ant_AntiniumQueen;
+
+        public static TraitDef Ant_HiveQueenTrait;
+
+        public static PawnRelationDef Ant_QueenRelation;
+        public static HediffDef Ant_HiveQueenHediff;
+        public static HediffDef Ant_HiveQueenInspHediff;
+        public static ThingDef Ant_RoyalEgg;
+
+        public static FactionDef Ant_GenTwoFaction;
+        public static FactionDef Ant_QueenFaction;
+
+        //public static GameCondition GameCondition_HiveQueen;
+        // public static GameCondition GameCondition_HiveQueenDied;
+
+    }
+
+}
